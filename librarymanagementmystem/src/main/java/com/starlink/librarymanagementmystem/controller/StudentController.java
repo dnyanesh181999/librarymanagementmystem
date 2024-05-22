60924d4ec5b1c15b50610d142da28c27f0671b54
Audumbar Kalokhe <audumbar.mescorp@gmail.com>
2024-05-22 23:16:08 +0530

    First Commit by shubham kalokhe

--- librarymanagementmystem/src/main/java/com/starlink/librarymanagementmystem/controller/StudentController.java
+++ librarymanagementmystem/src/main/java/com/starlink/librarymanagementmystem/controller/StudentController.java
@@ -1,5 +1,20 @@
 package com.starlink.librarymanagementmystem.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.starlink.librarymanagementmystem.model.Student;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
 
+	@PostMapping("/student")
+	public Student saveStudent(@RequestBody Student s) {
+		System.out.println("Student saved sucessfully");
+		return null;
+	}
+	
 }
